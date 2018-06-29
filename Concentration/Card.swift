79c46b499d9fdf5e2fe8b5f23e4611348e9f3fd8
Chase Hoselle <choselle@yahoo.com>
2018-06-28 21:37:20 -0500

    Initial Commit

---
+++ Concentration/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Chase Hoselle on 6/27/18.
+//  Copyright © 2018 Chibz. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        Card.identifierFactory += 1
+        return Card.identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
